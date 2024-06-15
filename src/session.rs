f446906f75d6a7a2a688a2d92610a49218de90e7
Ammar Arif <evergreenkary@gmail.com>
2024-06-15 02:08:48 -0400

    tests

--- src/session.rs
+++ src/session.rs
@@ -12,13 +12,14 @@ use tower_http::cors::CorsLayer;
 use tracing::trace;
 use url::Url;
 
-use crate::credential::{self, Credentials, SLOT_DIR};
+use crate::credential::{self, Credentials};
+use crate::utils::{self};
 use crate::{browser, server::LocalServer};
 
 const SESSION_CREATION_PAGE: &str = "https://x.cartridge.gg/slot/session";
 const SESSION_FILE_BASE_NAME: &str = "session.json";
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Default, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct Policy {
     /// The target contract address.
     pub target: FieldElement,
@@ -26,7 +27,7 @@ pub struct Policy {
     pub method: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct SessionDetails {
     /// The expiration date of the session.
@@ -36,7 +37,7 @@ pub struct SessionDetails {
     pub credentials: SessionCredentials,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Default, Serialize, Deserialize, PartialEq, Eq)]
 #[serde(rename_all = "camelCase")]
 pub struct SessionCredentials {
     /// The signing key of the session.
@@ -67,16 +68,7 @@ pub enum Error {
 /// This function will return an error if there is no authenticated user.
 ///
 pub fn get(chain: FieldElement) -> Result<Option<SessionDetails>, Error> {
-    let credentials = Credentials::load()?;
-    let username = credentials.account.expect("id must exist").id;
-    let path = get_file_path(&username, chain);
-
-    if path.exists() {
-        let contents = fs::read_to_string(path)?;
-        Ok(Some(serde_json::from_str(&contents)?))
-    } else {
-        Ok(None)
-    }
+    get_at(utils::config_dir(), chain)
 }
 
 /// Stores the session on-disk. Returns the path to the file where the `session` has been written to.
@@ -86,23 +78,7 @@ pub fn get(chain: FieldElement) -> Result<Option<SessionDetails>, Error> {
 /// This function will return an error if there is no authenticated user.
 ///
 pub fn store(chain: FieldElement, session: &SessionDetails) -> Result<PathBuf, Error> {
-    // TODO: maybe can store the authenticated user in a global variable so that
-    // we don't have to call load again if we already did it before.
-    let credentials = Credentials::load()?;
-    let username = credentials.account.expect("id must exist").id;
-    let path = get_file_path(&username, chain);
-
-    // Create the parent directories if they don't yet exist.
-    if let Some(parent) = path.parent() {
-        if !parent.exists() {
-            fs::create_dir_all(&path)?;
-        }
-    }
-
-    let contents = serde_json::to_string_pretty(session)?;
-    fs::write(&path, contents)?;
-
-    Ok(path)
+    store_at(utils::config_dir(), chain, session)
 }
 
 /// Creates a new session token. This will open a browser to the Cartridge Controller keychain page
@@ -125,6 +101,54 @@ where
     let mut rx = open_session_creation_page(&username, rpc_url.as_str(), policies)?;
 
     Ok(rx.recv().await.context("Channel dropped.")?)
+}
+
+fn get_at<P>(config_dir: P, chain: FieldElement) -> Result<Option<SessionDetails>, Error>
+where
+    P: AsRef<Path>,
+{
+    let credentials = Credentials::load_at(&config_dir)?;
+    let username = credentials.account.expect("id must exist").id;
+
+    let user_path = get_user_relative_file_path(&username, chain);
+    let file_path = config_dir.as_ref().join(user_path);
+
+    if file_path.exists() {
+        let contents = fs::read_to_string(file_path)?;
+        let session = serde_json::from_str(&contents)?;
+        Ok(Some(session))
+    } else {
+        Ok(None)
+    }
+}
+
+fn store_at<P>(
+    config_dir: P,
+    chain: FieldElement,
+    session: &SessionDetails,
+) -> Result<PathBuf, Error>
+where
+    P: AsRef<Path>,
+{
+    // TODO: maybe can store the authenticated user in a global variable so that
+    // we don't have to call load again if we already did it before.
+    let credentials = Credentials::load_at(&config_dir)?;
+    let username = credentials.account.expect("id must exist").id;
+
+    let path = get_user_relative_file_path(&username, chain);
+    let file_path = config_dir.as_ref().join(path);
+
+    // Create the parent directories if they don't yet exist.
+    if let Some(parent) = file_path.parent() {
+        if !parent.exists() {
+            fs::create_dir_all(parent)?;
+        }
+    }
+
+    let contents = serde_json::to_string_pretty(session)?;
+    fs::write(&file_path, contents)?;
+
+    Ok(file_path)
 }
 
 /// Starts the session creation process by opening the browser to the Cartridge keychain to prompt
@@ -173,7 +197,7 @@ fn prepare_query_params(
 }
 
 /// Create the callback server that will receive the session token from the browser.
-fn callback_server(tx: Sender<SessionDetails>) -> anyhow::Result<LocalServer> {
+pub(super) fn callback_server(tx: Sender<SessionDetails>) -> anyhow::Result<LocalServer> {
     let handler = move |tx: State<Sender<SessionDetails>>, session: Json<SessionDetails>| async move {
         trace!("Received session token from the browser.");
         tx.0.send(session.0).await.expect("qed; channel closed");
@@ -186,30 +210,40 @@ fn callback_server(tx: Sender<SessionDetails>) -> anyhow::Result<LocalServer> {
     Ok(LocalServer::new(router)?.cors(CorsLayer::permissive()))
 }
 
-fn get_file_path(username: &str, chain_id: FieldElement) -> PathBuf {
-    // eg 0x12345-session.json
+fn get_user_relative_file_path(username: &str, chain_id: FieldElement) -> PathBuf {
     let file_name = format!("{chain_id:#x}-{}", SESSION_FILE_BASE_NAME);
-
-    #[cfg(not(test))]
-    let mut path = dirs::config_local_dir().expect("unsupported OS");
-    #[cfg(test)]
-    let mut path = tempfile::tempdir().unwrap().into_path();
-
-    path.extend([SLOT_DIR, username, &file_name]);
-    path
+    PathBuf::from(username).join(file_name)
 }
 
 #[cfg(test)]
 mod tests {
     use super::{get, Error};
-    use crate::credential::Error::Unauthorized;
+    use crate::credential::{AccessToken, Credentials, Error::Unauthorized};
+    use crate::graphql::auth::me::{MeMe, MeMeCredentials};
+    use crate::session::{get_at, store_at, SessionDetails};
+    use crate::utils;
     use starknet::{core::types::FieldElement, macros::felt};
+    use std::path::Path;
+
+    fn authenticate(config_dir: impl AsRef<Path>) {
+        let token = AccessToken {
+            token: "mytoken".to_string(),
+            r#type: "Bearer".to_string(),
+        };
+
+        let me = MeMe {
+            name: None,
+            id: "foo".to_string(),
+            contract_address: None,
+            credentials: MeMeCredentials { webauthn: None },
+        };
+
+        let cred = Credentials::new(Some(me), token);
+        let _ = Credentials::store_at(&config_dir, &cred).unwrap();
+    }
 
     #[test]
-    fn get_non_existant_session() {}
-
-    #[test]
-    fn get_session_when_not_authenticated() {
+    fn get_session_unauthenticated() {
         let chain = FieldElement::ONE;
         let err = get(chain).unwrap_err();
         let Error::Credentials(Unauthorized) = err else {
@@ -218,5 +252,36 @@ mod tests {
     }
 
     #[test]
-    fn store_session() {}
+    fn get_non_existant_session_authenticated() {
+        let config_dir = utils::config_dir();
+        authenticate(&config_dir);
+
+        let chain = felt!("0x999");
+        let result = get_at(config_dir, chain).unwrap();
+        assert!(result.is_none())
+    }
+
+    #[test]
+    fn get_existant_session_authenticated() {
+        let config_dir = utils::config_dir();
+        authenticate(&config_dir);
+
+        let chain = felt!("0x999");
+        let expected = SessionDetails::default();
+        store_at(&config_dir, chain, &expected).unwrap();
+
+        let actual = get_at(config_dir, chain).unwrap();
+        assert_eq!(Some(expected), actual)
+    }
+
+    #[test]
+    fn store_session_unauthenticated() {
+        let config_dir = utils::config_dir();
+
+        let chain = felt!("0x999");
+        let session = SessionDetails::default();
+
+        let err = store_at(config_dir, chain, &session).unwrap_err();
+        assert!(err.to_string().contains("No credentials found"))
+    }
 }
