7042f7ac1c55f86300d1c54c7abc41acd880ad49
Ammar Arif <evergreenkary@gmail.com>
2024-06-15 02:10:18 -0400

    remove unused env var

--- src/constant.rs
+++ src/constant.rs
@@ -1,6 +1,3 @@
 // TODO: env instead?
 pub const CARTRIDGE_API_URL: &str = "https://api.cartridge.gg/";
 pub const CARTRIDGE_KEYCHAIN_URL: &str = "https://x.cartridge.gg/";
-
-/// The environment variable that can be used to override the default Slot credentials root directory.
-pub const SLOT_CREDENTIALS_ROOT_DIR_ENV_VAR: &str = "SLOT_CREDENTIALS_ROOT_DIR";
