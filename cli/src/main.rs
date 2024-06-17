f51ab2df7683fdcfdf023685cfb7793336b7ed79
Ammar Arif <evergreenkary@gmail.com>
2024-06-17 10:38:49 -0400

    Convert into workspace

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
 mod command;
 
 use crate::command::Command;
--- slot/src/lib.rs
+++ slot/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(test), warn(unused_crate_dependencies))]
+
 pub mod api;
 pub mod browser;
 pub mod constant;
