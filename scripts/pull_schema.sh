418e7ce8f959c36e43b2229b2848ee38e82fd8ba
Luca Steeb <contact@luca-steeb.com>
2024-08-19 15:38:51 +0100

    Add graphql config for syntax highlighting, add binary docs (#70)

--- scripts/pull_schema.sh
+++ scripts/pull_schema.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-graphql-client introspect-schema --output schema.json https://api.cartridge.gg/query
+# https://github.com/graphql-rust/graphql-client/blob/main/graphql_client_cli/README.md
+# cargo install graphql_client_cli
+
+graphql-client introspect-schema --output slot/schema.json https://api.cartridge.gg/query
