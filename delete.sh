ba51bef7ca04dccbc216a42abbc4b893c88b134c
steebchen <contact@luca-steeb.com>
2024-09-27 11:55:30 -0400

    Remove delete.sh script
    The delete.sh script was responsible for deleting specific deployments sequentially. Given recent changes in deployment practices, this script is now obsolete and no longer necessary in the repository.

--- delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for n in {1..100}; do
-    cargo run -- deployments delete "ls-tmp-$n" torii -f
-    cargo run -- deployments delete "ls-tmp-$n" katana -f
-    cargo run -- deployments delete "ls-tmp-$n" saya -f
-done
