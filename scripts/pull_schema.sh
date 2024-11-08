622140126f5f5627dd1233cdd4ba925cb17118bb
steebchen <contact@luca-steeb.com>
2024-11-09 00:39:09 +0700

    feat(torii): add config file support for Torii deployments
    Added optional config_file parameter to Torii deployments. New
    files read in base64 and included in the deployment request.
    Updated corresponding GraphQL schema and dependencies.

--- scripts/pull_schema.sh
+++ scripts/pull_schema.sh
@@ -3,5 +3,5 @@
 # https://github.com/graphql-rust/graphql-client/blob/main/graphql_client_cli/README.md
 # cargo install graphql_client_cli
 
-graphql-client introspect-schema --output slot/schema.json https://api.cartridge.gg/query
-# graphql-client introspect-schema --output slot/schema.json http://localhost:8000/query
+#graphql-client introspect-schema --output slot/schema.json https://api.cartridge.gg/query
+graphql-client introspect-schema --output slot/schema.json http://localhost:8000/query
