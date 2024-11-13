aa3f9f7ead03506d851eaf2294ab6cba0ba7b89b
glihm <dev@glihm.net>
2024-11-13 16:40:58 -0600

    refactor: remove fork command

--- cli/src/command/deployments/mod.rs
+++ cli/src/command/deployments/mod.rs
@@ -4,14 +4,13 @@ use colored::*;
 
 use self::{
     accounts::AccountsArgs, create::CreateArgs, delete::DeleteArgs, describe::DescribeArgs,
-    fork::ForkArgs, list::ListArgs, logs::LogsArgs, update::UpdateArgs,
+    list::ListArgs, logs::LogsArgs, update::UpdateArgs,
 };
 
 mod accounts;
 mod create;
 mod delete;
 mod describe;
-mod fork;
 mod list;
 mod logs;
 mod services;
@@ -25,8 +24,6 @@ pub enum Deployments {
     Delete(DeleteArgs),
     #[command(about = "Update a deployment.")]
     Update(UpdateArgs),
-    #[command(about = "Fork a deployment.")]
-    Fork(ForkArgs),
     #[command(about = "Describe a deployment's configuration.")]
     Describe(DescribeArgs),
     #[command(about = "List all deployments.", aliases = ["ls"])]
@@ -43,7 +40,6 @@ impl Deployments {
             Deployments::Create(args) => args.run().await,
             Deployments::Delete(args) => args.run().await,
             Deployments::Update(args) => args.run().await,
-            Deployments::Fork(args) => args.run().await,
             Deployments::Describe(args) => args.run().await,
             Deployments::List(args) => args.run().await,
             Deployments::Logs(args) => args.run().await,
