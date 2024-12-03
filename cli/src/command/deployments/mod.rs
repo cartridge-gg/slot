cc144cdc9cb67c0f8aeea0cb95b5359fc129b475
steebchen <contact@luca-steeb.com>
2024-12-03 17:15:21 +0700

    fix(deployments): remove forking functionality
    Removed the fork functionality from the deployments
    module, including associated files and references. This
    streamlines the deployment options available within the
    application.

--- cli/src/command/deployments/mod.rs
+++ cli/src/command/deployments/mod.rs
@@ -4,14 +4,13 @@ use colored::*;
 
 use self::{
     accounts::AccountsArgs, create::CreateArgs, delete::DeleteArgs, describe::DescribeArgs,
-    fork::ForkArgs, list::ListArgs, logs::LogsArgs, update::UpdateArgs,
+    list::ListArgs, logs::LogsArgs, update::UpdateArgs,
 };
 
 mod accounts;
 mod create;
 mod delete;
 mod describe;
-mod fork;
 mod list;
 mod logs;
 mod services;
@@ -27,8 +26,6 @@ pub enum Deployments {
     Delete(DeleteArgs),
     #[command(about = "Update a deployment.")]
     Update(UpdateArgs),
-    #[command(about = "Fork a deployment.")]
-    Fork(ForkArgs),
     #[command(about = "Describe a deployment's configuration.")]
     Describe(DescribeArgs),
     #[command(about = "List all deployments.", aliases = ["ls"])]
@@ -45,7 +42,6 @@ impl Deployments {
             Deployments::Create(args) => args.run().await,
             Deployments::Delete(args) => args.run().await,
             Deployments::Update(args) => args.run().await,
-            Deployments::Fork(args) => args.run().await,
             Deployments::Describe(args) => args.run().await,
             Deployments::List(args) => args.run().await,
             Deployments::Logs(args) => args.run().await,
