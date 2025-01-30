603788cdcb756a2a63dea8332645d758e7113d3b
Luca Steeb <contact@luca-steeb.com>
2025-01-30 13:32:24 +0700

    chore(scripts): add param for local/prod schema pull (#161)
    Allow specifying "local" or "prod" to pull schema from the desired
    environment. Display usage instructions for invalid inputs.

--- scripts/pull_schema.sh
+++ scripts/pull_schema.sh
@@ -3,5 +3,11 @@
 # https://github.com/graphql-rust/graphql-client/blob/main/graphql_client_cli/README.md
 # cargo install graphql_client_cli
 
-graphql-client introspect-schema --output slot/schema.json https://api.cartridge.gg/query
-#graphql-client introspect-schema --output slot/schema.json http://localhost:8000/query
+# check if param to pull schema is set to "local"
+if [ "$1" = "local" ]; then
+	graphql-client introspect-schema --output slot/schema.json http://localhost:8000/query
+elif [ "$1" = "prod" ]; then
+	graphql-client introspect-schema --output slot/schema.json https://api.cartridge.gg/query
+else
+	echo "usage: pull_schema.sh [local|prod]"
+fi
