11bdaf5e2d28d8f199fa1f9d0d948ae103c9ee45
Nasr <larko236@outlook.com>
2025-03-14 21:08:12 +0900

    create team

--- cli/src/command/teams/create.rs
+++ cli/src/command/teams/create.rs
@@ -5,6 +5,7 @@ use slot::credential::Credentials;
 use slot::graphql::GraphQLQuery;
 use slot::graphql::team::CreateTeam;
 use slot::graphql::team::team_member_add::Variables;
+use slot::graphql::team::create_team;
 
 #[derive(Debug, Args, serde::Serialize)]
 #[command(next_help_heading = "Team create options")]
@@ -15,14 +16,14 @@ pub struct CreateTeamArgs {
 
 impl CreateTeamArgs {
     pub async fn run(&self, team: String) -> Result<()> {
-        let request_body = CreateTeam::build_query(Variables {
-            name: team,
+        let request_body = CreateTeam::build_query(create_team::Variables {
+            name: self.name.clone(),
         });
 
         let credentials = Credentials::load()?;
         let client = Client::new_with_token(credentials.access_token);
 
-        let data: ResponseData = client.query(&request_body).await?;
+        let data: create_team::ResponseData = client.query(&request_body).await?;
 
         println!("Team created successfully 🚀");
 
