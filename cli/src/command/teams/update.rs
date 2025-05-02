4c0a69522434821af89b01044476725a0c42f6d0
Luca Steeb <contact@luca-steeb.com>
2025-05-02 19:38:20 +0700

    feat(teams): add update functionality for teams (#238)
    Introduced `updateTeam` mutation and CLI command to update team
    details such as email. Updated schema and GraphQL queries to support
    new fields.

--- cli/src/command/teams/update.rs
+++ cli/src/command/teams/update.rs
@@ -2,33 +2,33 @@ use anyhow::Result;
 use clap::Args;
 use slot::api::Client;
 use slot::credential::Credentials;
-use slot::graphql::team::create_team;
-use slot::graphql::team::CreateTeam;
+use slot::graphql::team::update_team;
+use slot::graphql::team::UpdateTeam;
 use slot::graphql::GraphQLQuery;
 
 #[derive(Debug, Args, serde::Serialize)]
-#[command(next_help_heading = "Team create options")]
-pub struct CreateTeamArgs {
+#[command(next_help_heading = "Team update options")]
+pub struct UpdateTeamArgs {
     #[arg(long)]
     #[arg(help = "The email address for team notifications.")]
     pub email: Option<String>,
 }
 
-impl CreateTeamArgs {
+impl UpdateTeamArgs {
     pub async fn run(&self, team: String) -> Result<()> {
-        let request_body = CreateTeam::build_query(create_team::Variables {
+        let request_body = UpdateTeam::build_query(update_team::Variables {
             name: team.clone(),
-            input: Some(create_team::TeamInput {
+            input: update_team::TeamInput {
                 email: self.email.clone(),
-            }),
+            },
         });
 
         let credentials = Credentials::load()?;
         let client = Client::new_with_token(credentials.access_token);
 
-        let data: create_team::ResponseData = client.query(&request_body).await?;
+        let _data: update_team::ResponseData = client.query(&request_body).await?;
 
-        println!("Team {} created successfully 🚀", data.create_team.name);
+        println!("Team {} updated successfully 🚀", team);
 
         Ok(())
     }
--- slot/src/graphql/team/mod.rs
+++ slot/src/graphql/team/mod.rs
@@ -7,6 +7,13 @@ use graphql_client::GraphQLQuery;
     query_path = "src/graphql/team/create.graphql"
 )]
 pub struct CreateTeam;
+#[derive(GraphQLQuery)]
+#[graphql(
+    response_derives = "Debug",
+    schema_path = "schema.json",
+    query_path = "src/graphql/team/update.graphql"
+)]
+pub struct UpdateTeam;
 
 #[derive(GraphQLQuery)]
 #[graphql(
