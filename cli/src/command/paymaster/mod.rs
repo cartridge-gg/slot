2eaa5cfecff5556e5b773e81b336eed2c66f8ba0
broody <broody@gmail.com>
2025-06-09 11:05:57 -1000

    feat(paymaster): transactions query

--- cli/src/command/paymaster/mod.rs
+++ cli/src/command/paymaster/mod.rs
@@ -10,6 +10,7 @@ use self::info::InfoArgs;
 use self::policy::PolicyCmd;
 use self::stats::StatsArgs;
 use self::update::UpdateArgs;
+use self::transactions::TransactionArgs;
 
 mod budget;
 mod create;
@@ -17,6 +18,8 @@ mod info;
 mod policy;
 mod stats;
 mod update;
+mod transactions;
+mod utils;
 
 #[derive(Debug, Args, Serialize, Deserialize)]
 pub struct PolicyArgs {
@@ -60,6 +63,9 @@ enum PaymasterSubcommand {
 
     #[command(about = "Get paymaster info.")]
     Info(InfoArgs),
+
+    #[command(about = "Get paymaster transactions.")]
+    Transactions(TransactionArgs),
 }
 
 impl PaymasterCmd {
@@ -73,6 +79,7 @@ impl PaymasterCmd {
             PaymasterSubcommand::Stats(cmd) => cmd.run(self.name.clone()).await,
             PaymasterSubcommand::Info(cmd) => cmd.run(self.name.clone()).await,
             PaymasterSubcommand::Update(cmd) => cmd.run(self.name.clone()).await,
+            PaymasterSubcommand::Transactions(cmd) => cmd.run(self.name.clone()).await,
         }
     }
 }
